dd62d6cb790808993e8c5c22c93ef7b3297991d8
Evin Ugur <evinugur96@gmail.com>
2014-08-27 02:23:55 -0400

    base commit

---
+++ README.md
@@ -0,0 +1,8 @@
+<a href = "https://chrome.google.com/webstore/detail/hpkhecccdfdgcmlmhdflfhdffebckjgf/"><img src = "http://evinugur.com/chrome.png"></a>
+
+#No Spoilers on *abc* **get** TV streaming sites
+
+
+
+This chrome extension removes episode titles, thumbnails, and the embedded chats from get sites that offer TV shows with the intention of creating a spoiler free browsing experience.
+For long series, even so much as an episode name can reveal what happens way down the line and can skew your perception on how the events of a show are going to unfold... not anymore!
---
+++ dommanipulation.js
@@ -0,0 +1,22 @@
+document.addEventListener("load", function() {
+	document.onscroll = function(){
+		window.defined = undefined;
+		var bliss = "http://evinugur.com/dorite/doritesummerbreeze.gif";
+		var whoaman = "http://evinugur.com/dorite/splashfire.jpg";
+		var classes = ['_s0 _5xib _5sq7', 'UFIActorImage', 'tickerStoryImage', '_s0 _rw'];
+		for (var i = 0; i < classes.length; i++) {
+			var arr = document.getElementsByClassName(classes[i]);
+			for (var j = 0; j < arr.length; j++) {
+				arr[j].src = bliss;
+			}
+		}
+		var tricky = document.getElementsByClassName("_55lt");
+		for (var i = 0; i < tricky.length; i++) {
+			tricky[i].children[0].src = bliss;
+		}
+		var style = document.createElement("style");
+		style.innerHTML  = "#globalContainer{background-repeat: repeat; background-image: url(" + whoaman + ");}";
+		document.head.appendChild(style);
+	};
+	
+}, true);

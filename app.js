87038435db05f31f0386f3c826dd7236ba226b7a
Evin Ugur <evinugur96@gmail.com>
2014-08-27 11:19:22 -0400

    removed src from gh-pages branch

--- app.js
+++
@@ -1,6 +0,0 @@
-var script = document.createElement('script');
-script.src = chrome.extension.getURL('dommanipulation.js');
-script.onload = function() {
-    this.parentNode.removeChild(this);
-};
-(document.head || document.documentElement).appendChild(script);

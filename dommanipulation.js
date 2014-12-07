805b7fdab2c2e4ecd41c20ef829402105d04393f
Evin Ugur <evinoog96@gmail.com>
2014-12-06 20:56:34 -0500

    1.5 Release of DoriteSummerRain special thanks to @petermitrano for identifting the crippling bug

--- dommanipulation.js
+++ dommanipulation.js
@@ -1,6 +1,5 @@
 document.addEventListener("load", function() {
 	document.onscroll = function(){
-		window.defined = undefined;
 		var bliss = "http://evinugur.com/dorite/doritesummerbreeze.gif";
 		var whoaman = "http://evinugur.com/dorite/splashfire.jpg";
 		var classes = ['_s0 _5xib _5sq7', 'UFIActorImage', 'tickerStoryImage', '_s0 _rw', '_s0 _rw', 'profilePhoto', '_s0 _50c7 _54rt'];
